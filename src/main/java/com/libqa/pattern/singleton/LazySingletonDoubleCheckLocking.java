35d4e65e9548bd7d7a6762ad5e02d1d632ff249e
yionlee <performizer@gmail.com>
2016-09-17 16:40:42 +0900

    디자인 패턴 study - Singleton Pattern

---
+++ src/main/java/com/libqa/pattern/singleton/LazySingletonDoubleCheckLocking.java
@@ -0,0 +1,28 @@
+package com.libqa.pattern.singleton;
+
+/**
+ * @Author : yion
+ * @Date : 2016. 9. 17.
+ * @Description :
+ */
+public class LazySingletonDoubleCheckLocking {
+    private static volatile LazySingletonDoubleCheckLocking instance = null;
+
+    private LazySingletonDoubleCheckLocking(){
+
+    }
+
+    public static LazySingletonDoubleCheckLocking getInstance() {
+        if (instance == null) {
+            synchronized (LazySingletonDoubleCheckLocking.class) {
+                // Double check locking
+                if (instance == null) {
+                    instance = new LazySingletonDoubleCheckLocking();
+                }
+            }
+        }
+
+        return instance;
+
+    }
+}
